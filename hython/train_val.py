ad1e3fa4e31173e36f9ac33422221454876180cc
iacopoff <iacopo.ff@gmail.com>
2024-05-20 21:08:53 +0200

    trainer

--- hython/train_val.py
+++ hython/train_val.py
@@ -4,164 +4,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm.auto import tqdm
 
-# get the current learning rate
-def get_lr(opt):
-    for param_group in opt.param_groups:
-        return param_group["lr"]
 
+def train_val(trainer, model, epochs, optimizer, lr_scheduler, dp_weights, device):
 
-# Define the Loss_batch function
-def loss_batch(loss_func, output, target, opt=None):
-    if target.shape[-1] == 1:
-        target = torch.squeeze(target)
-        output = torch.squeeze(output)
+    target_names = trainer.P.target_names 
     
-    loss = loss_func(target, output)
-    if opt is not None: # evaluation
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-
-    return loss 
-
-
-# Define the metric_epoch function
-def metric_epoch(metric_func, y_pred, y_true, target_names):
-    metrics = metric_func(y_pred, y_true, target_names) 
-    return metrics
-
-
-# Define the loss_epoch function
-def loss_epoch(model, loss_func, metric_func, dataset_dl, target_names, device, opt=None, ts_idx= None, seq_length = None):
-    running_loss = 0
+    loss_history = {"train": [], "val": []}    
+    metric_history = {f'train_{target}': [] for target in target_names}
+    metric_history.update({f'val_{target}': [] for target in target_names})
     
-    spatial_sample_size = 0 
-    
-    epoch_preds = None
-    epoch_targets = None 
-
-    for (
-        predictors_b,
-        static_params_b,
-        targets_b,
-    ) in dataset_dl:  # batch_predictors, batch_static_params, batch_targets
-        
-        running_time_batch_loss = 0
-
-        for t in ts_idx:
-
-            predictors_bt = predictors_b[:, t:(t + seq_length)].to(device)
-            static_params_bt = static_params_b.to(device)
-            targets_bt = targets_b[:, t:(t + seq_length)].to(device)
-            
-            output = model(predictors_bt, static_params_bt)[:, -1] # take last time step
-
-            if epoch_preds is None:
-                epoch_preds = output.detach().cpu().numpy()
-                epoch_targets = targets_bt[:, -1].detach().cpu().numpy()
-            else:
-                epoch_preds = np.concatenate(
-                    (epoch_preds, output.detach().cpu().numpy()), axis=0
-                )
-                epoch_targets = np.concatenate(
-                    (epoch_targets, targets_bt[:, -1].detach().cpu().numpy()), axis=0
-                )
-
-            # get loss per i time batch
-            loss_time_batch = loss_batch(loss_func, output, targets_bt[:, -1], opt) 
-
-            # update running loss
-            running_time_batch_loss += loss_time_batch #* targets_bt.size(0)
-
-        #running_time_batch_loss /= len(ts_idx)
-        
-        # accumulate number of samples
-        spatial_sample_size += targets_b.size(0)
-
-        running_loss += running_time_batch_loss
-
-    # average loss value
-    loss = running_loss / spatial_sample_size
-
-    # average metric value
-    metric = metric_epoch(metric_func, epoch_targets, epoch_preds, target_names)
-    
-    return loss, metric
-
-
-def train_val(model, params):
-    num_epochs = params["num_epochs"]
-    seq_length = params["seq_length"]
-    temporal_sampling_size = params["temporal_sampling_size"]
-    temporal_sampling_epoch = params["temporal_sampling_idx_change_with_epoch"]
-    ts_range = params["ts_range"]
-    loss_func = params["loss_func"]
-    metric_func = params["metric_func"]
-    target_names = params["target_names"]
-    opt = params["optimizer"]
-    train_dl = params["train_dl"]
-    val_dl = params["val_dl"]
-    lr_scheduler = params["lr_scheduler"]
-    path2weights = params["path2weights"]
-    device = params["device"]
-
-    loss_history = {"train": [], "val": []}
-
-    metric_history = {f'train_{t}': [] for t in target_names}
-    metric_history.update({f'val_{t}': [] for t in target_names})
-
-    best_model_wts = copy.deepcopy(model.state_dict())
     best_loss = float("inf")
-
-    if not temporal_sampling_epoch:
-        ts_idx = np.random.randint(0,ts_range  - seq_length, temporal_sampling_size)
     
-    for epoch in tqdm(range(num_epochs)):
-        current_lr = get_lr(opt)
+    for epoch in tqdm(range(epochs)):
         
-        print(f"Epoch {epoch}/{num_epochs - 1}, current lr={current_lr}")
-        
-        # every epoch generate a new set of random time indices (sampling the timeseries)
-        if temporal_sampling_epoch:
-            ts_idx = np.random.randint(0, ts_range  - seq_length, temporal_sampling_size)
+        trainer.temporal_index() # For RNNs based models
 
-    
         model.train()
-        train_loss, train_metric = loss_epoch(
-            model, loss_func, metric_func, train_dl, target_names, device, opt, ts_idx, seq_length
-        )
-
-        loss_history["train"].append(train_loss)
-        for t in target_names: metric_history[f'train_{t}'].append(train_metric[t])
+        
+        train_loss, train_metric = trainer.epoch_step(model, device, opt = optimizer)
 
         model.eval()
         with torch.no_grad():
-            val_loss, val_metric = loss_epoch(
-                model, loss_func, metric_func, val_dl, target_names, device, ts_idx= ts_idx, seq_length = seq_length
-            )
+            val_loss, val_metric = trainer.epoch_step(model, device, opt= None)
+        
+        lr_scheduler.step(val_loss)
 
+        loss_history["train"].append(train_loss)
         loss_history["val"].append(val_loss)
-        for t in target_names: metric_history[f'val_{t}'].append(val_metric[t])
+ 
+        for target in target_names: 
+            metric_history[f'train_{target}'].append(train_metric[target])
+            metric_history[f'val_{target}'].append(val_metric[target])
 
         if val_loss < best_loss:
             best_loss = val_loss
-            best_model_wts = copy.deepcopy(model.state_dict())
-            torch.save(model.state_dict(), path2weights)
+            best_model_weights = copy.deepcopy(model.state_dict())
+            trainer.save_weights(model, dp_weights)
             print("Copied best model weights!")
 
-        lr_scheduler.step(val_loss)
+        print(f"train loss: {train_loss}")
+        print(f"val loss: {val_loss}")
 
-        # lr_scheduler.step()
-        #if current_lr != get_lr(opt):
-        #    print("Loading best model weights!")
-        #    model.load_state_dict(best_model_wts)
-
-        print(f"train loss: {train_loss}, train metric: {train_metric}")
-        print(f"val loss: {val_loss}, val metric: {val_metric}")
-        print("-" * 10)
-        
-
-
-    model.load_state_dict(best_model_wts)
-    
+    model.load_state_dict(best_model_weights)
+            
     return model, loss_history, metric_history
---
+++ hython/trainer.py
@@ -0,0 +1,132 @@
+import torch
+import numpy as np
+from dataclasses import dataclass
+from torch.utils.data import DataLoader
+
+@dataclass
+class BaseTrainParams:
+    experiment:str
+    # blabla
+    loss_func:callable
+    metric_func:callable
+    # blabla
+    train_dataloader:DataLoader
+    val_dataloader:DataLoader
+
+@dataclass
+class RNNTrainParams(BaseTrainParams):
+    # blabla
+    subsample:bool 
+    n_subsample:int
+    seq_length:int
+    # blabla
+    time_range:int
+    # blabla
+    target_names:str
+
+class BaseTrainer:
+
+    def __init__(self, experiment):
+        self.exp = experiment
+
+    def temporal_index(self):
+        pass
+
+    def epoch_step(self):
+        pass
+
+    def predict_step(self):
+        pass
+
+    def save_weights(self, model, dp):
+        torch.save(model.state_dict(), f"{dp}/{self.exp}")
+
+def metric_epoch(metric_func, y_pred, y_true, target_names):
+    metrics = metric_func(y_pred, y_true, target_names) 
+    return metrics
+
+def loss_batch(loss_func, output, target, opt=None):
+    if target.shape[-1] == 1:
+        target = torch.squeeze(target)
+        output = torch.squeeze(output)
+    
+    loss = loss_func(target, output)
+    if opt is not None: # evaluation
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+    return loss 
+
+class RNNTrainer(BaseTrainer):
+    
+    def __init__(self, params):
+        self.P = RNNTrainParams(**params)
+        super(RNNTrainer, self).__init__(self.P.experiment)
+        
+        
+    def temporal_index(self):
+        """Return the temporal indices of the timeseries, it may be a subset"""
+        if self.P.subsample:
+            self.time_index = np.random.randint(0, self.P.time_range  - self.P.seq_length, self.P.n_subsample)
+        else:
+            self.time_index = np.arange(0, self.P.time_range)
+        self.time_index
+
+    def epoch_step(self, model, device, opt = None):
+        if opt:
+            dataloader = self.P.train_dataloader
+        else:
+            dataloader = self.P.val_dataloader
+
+        running_loss = 0
+        spatial_sample_size = 0 
+        
+        epoch_preds = None
+        epoch_targets = None 
+        
+        for (dynamic_b, static_b, targets_b) in dataloader:
+
+            running_time_batch_loss = 0
+            for t in self.time_index: # time_index could be a subset of time indices 
+                dynamic_bt = dynamic_b[:, t:(t + self.P.seq_length)].to(device)
+                static_bt = static_b.to(device)
+                targets_bt = targets_b[:, t:(t + self.P.seq_length)].to(device)
+
+                output = model(dynamic_bt, static_bt)
+
+                output = self.predict_step(output)
+                target = self.predict_step(targets_bt)
+
+                if epoch_preds is None:
+                    epoch_preds = output.detach().cpu().numpy()
+                    epoch_targets = target.detach().cpu().numpy()
+                else:
+                    epoch_preds = np.concatenate(
+                        (epoch_preds, output.detach().cpu().numpy()), axis=0
+                    )
+                    epoch_targets = np.concatenate(
+                        (epoch_targets, target.detach().cpu().numpy()), axis=0
+                    )
+
+                loss_time_batch = loss_batch(self.P.loss_func, output, target, opt) 
+
+                running_time_batch_loss += loss_time_batch
+
+            spatial_sample_size += targets_b.size(0)
+
+            running_loss += running_time_batch_loss
+
+        loss = running_loss / spatial_sample_size
+
+        metric = metric_epoch(self.P.metric_func, epoch_targets, epoch_preds, self.P.target_names)
+            
+        return loss, metric
+        
+    def predict_step(self, arr):
+        """Return the n steps that should be predicted"""
+        return arr[:, -1]
+
+
+
+
