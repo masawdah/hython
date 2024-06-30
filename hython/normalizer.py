7d1ac847383f04e3a91d52af84217c04d473cad0
iacopoff <iacopo.ff@gmail.com>
2024-06-30 11:48:33 +0200

    update normalizer for xarray dataset

--- hython/normalizer.py
+++ hython/normalizer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xarray as xr
 from dask.array import expand_dims, nanmean, nanstd, nanmin, nanmax
 
 
@@ -10,22 +11,34 @@ SCALER = {
     "standardize": lambda arr, axis, m1, m2: (arr - expand_dims(m1, axis=axis)) / expand_dims(m2, axis=axis)
     }
 
+SCALER_XARRAY = {
+    "minmax": lambda arr, axis, m1, m2: (arr - m1)/ (m2 - m1),
+    "standardize": lambda arr, axis, m1, m2: (arr - m1)/ m2
+}
 
+DENORM_XARRAY = {
+    "standardize": lambda arr, m1, m2: (arr * m2) + m1
+}
 DENORM = {
     "standardize": lambda arr, axis, m1, m2:  (arr * expand_dims(m2, axis=axis)) + expand_dims(m1, axis=axis)
 }
 
-TYPE = {"1D":{"space": 0, "time": 1, "spacetime": (0, 1)} , # N T C
-        "2D":{"space": (1, 2), "time": 1, "spacetime": (1, 2, 3)} } # C T H W
+TYPE = {
+    "NTC":{    "space": 0,     "time": 1,  "spacetime": (0, 1)} ,   # N T C
+    "NCTHW":{  "space": (1,2), "time": 1,  "spacetime": (1, 2, 3)}, # C T H W
+    "NLCHW":{  "space": (2,3), "time": 0,  "spacetime": (0, 2, 3)},
+    "xarray_dataset":{"space": ("lat","lon"), "time":("time"), "spacetime":("time", "lat", "lon")}
+}
 
 
 class Normalizer:
     def __init__(
-        self, method: str,  type: str, shape:str, save_stats: bool = False
+        self, method: str,  type: str, axis_order:str, dask_compute:bool = False, save_stats: bool = False
     ):
         self.method = method
-        self.shape = shape 
+        self.axis_order = axis_order
         self.type = type
+        self.dask_compute = dask_compute
         self.save_stats = save_stats
 
         self.stats_iscomputed = False
@@ -33,11 +46,16 @@ class Normalizer:
         self._set_axis()
 
     def _set_axis(self):
-        self.axis = TYPE.get(self.shape).get(self.type, False)
-        #self.axis = TYPE.get(self.type, False)
+        axis_order = TYPE.get(self.axis_order)
+
+        self.axis = axis_order.get(self.type, None)
 
     def _get_scaler(self):
-        scaler = SCALER.get(self.method, None)
+        if "xarray" in self.axis_order:
+            scaler = SCALER_XARRAY.get(self.method, None)
+        else:
+            scaler = SCALER.get(self.method, None)
+
         if scaler is None: raise NameError(f"Scaler for {self.method} does not exists") 
         else: return scaler
     
@@ -49,12 +67,23 @@ class Normalizer:
     def compute_stats(self, arr):
         
         print("compute stats")
+        if isinstance(arr, xr.Dataset):
+            if self.method == "standardize":
+                if self.dask_compute:
+                    self.computed_stats = [arr.mean(self.axis).compute(), arr.std(self.axis).compute()]
+                else:
+                    self.computed_stats = [arr.mean(self.axis), arr.std(self.axis)]
+            else:
+                if self.dask_compute:
+                    self.computed_stats = [arr.min(self.axis).compute(), arr.max(self.axis).compute()] 
+                else:
+                    self.computed_stats = [arr.min(self.axis), arr.max(self.axis)] 
+                
+        else:
+            funcs = self._get_funcs()
+            self.computed_stats =  [f(arr, axis=self.axis).compute() for f in funcs]
 
-        funcs = self._get_funcs()
-        
         self.stats_iscomputed = True
-
-        self.computed_stats =  [f(arr, axis=self.axis).compute() for f in funcs]
 
 
     def normalize(self, arr, read_from=None, write_to = None):
@@ -70,19 +99,23 @@ class Normalizer:
         if write_to is not None:
             self.write_stats(write_to)
 
-        return scale_func(arr, self.axis, *self.computed_stats)
+        if self.dask_compute:
+            return scale_func(arr, self.axis, *self.computed_stats).compute()
+        else:
+            return scale_func(arr, self.axis, *self.computed_stats)
 
     def denormalize(self, arr, fp=None):
         if self.method == "standardize":
-
-            func = DENORM.get(self.method)
+            if "xarray" in self.axis_order:
+                func = DENORM_XARRAY.get(self.method)
+            else:
+                func = DENORM.get(self.method)
             
             if fp is not None:
                 self.read_stats(fp)
                 m, std = self.computed_stats
                 return func(arr, self.axis, m, std)
             else:
-
                 m, std = self.computed_stats
                 return func(arr, self.axis, m, std)
         else:
@@ -90,12 +123,23 @@ class Normalizer:
 
     def write_stats(self, fp):
         print("save stats")
-        np.save(fp, self.computed_stats)
+        if "xarray" in self.axis_order:
+            xarr = xr.DataArray(["m1","m2"], coords=[("stats",["m1","m2"])])
+            ds = xr.concat(self.computed_stats, dim= xarr)
+            ds.to_netcdf(fp, mode="w")
+        else:
+            np.save(fp, self.computed_stats)
 
     def read_stats(self, fp):
         print("read stats")
         self.stats_iscomputed = True
-        self.computed_stats = np.load(fp)
+        if "xarray" in self.axis_order:
+            ds = xr.open_dataset(fp)
+            ds.close() # closing so that I can overwrite it with write stats
+            self.computed_stats = [ds.sel(stats="m1", drop=True), ds.sel(stats="m2", drop=True)]
+            
+        else:
+            self.computed_stats = np.load(fp)
 
     def get_stats(self):
         return self.computed_stats
