a794cd805ab5d457945aa52cad945ba112c6246f
iacopoff <iacopo.ff@gmail.com>
2024-08-08 13:50:53 +0200

    lsf lstm

--- hython/models/cudnnLSTM.py
+++ hython/models/cudnnLSTM.py
@@ -32,3 +32,59 @@ class CuDNNLSTM(nn.Module):
         out = self.fc1(lstm_output)
 
         return out
+
+
+
+class LSTMModule(nn.Module):
+    def __init__(
+        self,
+        hidden_size: int = 34,
+        input_size: int = 3,
+        num_layers:int = 1,
+        dropout:float = 0.0,
+    ):
+        super(LSTMModule, self).__init__()
+
+        self.fc0 = nn.Linear(input_size, hidden_size)
+
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers = num_layers, batch_first=True, dropout=dropout)
+
+    def forward(self, x):
+
+        l1 = self.fc0(x)
+
+        lstm_output, (h_n, c_n) = self.lstm(l1)
+
+        return lstm_output
+    
+
+class LandSurfaceLSTM(nn.Module):
+
+    def __init__(self, 
+                 module_dict,
+                 output_size,
+                 device):
+        super(LandSurfaceLSTM, self).__init__()
+        
+        self.modules = {k:LSTMModule(**v).to(device) for k,v in module_dict.items()}
+
+        total_hidden_size = sum([v["hidden_size"] for v in module_dict.values()])
+
+        self.fc0 = nn.Linear(total_hidden_size, output_size)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+
+        distr_out = [] 
+        for variable in self.modules:
+            distr_out.append(
+                self.modules[variable](x)
+            )
+            
+        out = torch.cat(distr_out, dim=-1)
+
+        # Interaction
+        out = self.relu(self.fc0(out))
+        
+        return out
